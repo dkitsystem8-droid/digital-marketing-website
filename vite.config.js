0a40eb57bebf54357addeb2bb638e114aa5d3069
demo <dkit.system8@gmail.com>
2026-01-05 15:51:18 +0530

    Initial commit: Added navbar, process section, and contact page

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Vercel deployment path
+  build: {
+    outDir: 'dist', // Vercel expects 'dist' folder
+    sourcemap: false, // optional, reduces build size
+  },
 })
